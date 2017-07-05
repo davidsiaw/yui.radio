afe3ad93d420940d811e895a537a562e499066df
David Siaw <davidsiaw@gmail.com>
2017-07-05 09:38:11 +0900

    make it so bot reconnects if dc

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ require 'yaml'
 @volume = 0.5
 @current_song = {}
 
-running = true
+@running = true
 client_id = ENV["CLIENT_ID"]
 default_ws_url = 'wss://listen.moe/api/v2/socket'
 @ws_url = ENV["WEBSOCKET_URL"] || default_ws_url
@@ -47,18 +47,18 @@ Discordrb::LOGGER.streams << stdout
 Thread.new do
   loop do
     log(stdout.readline, :dsout)
-    break if !running
+    break if !@running
   end
 end
 
 Signal.trap("INT") do
   log "Stopping..."
   EventMachine::stop_event_loop
-  if !running
+  if !@running
     log "Force stopping..."
     exit(1)
   end
-  running = false
+  @running = false
 end
 
 def start_bot!(client_id)
@@ -74,7 +74,7 @@ def start_bot!(client_id)
       tokens = event.message.content.split(" ")
       @volume = tokens[2].to_f
       event.respond("Okie~! Be right baack")
-      stop_bot!
+      EventMachine::stop_event_loop
     end
   end
 
@@ -97,15 +97,24 @@ Anime name: #{@current_song["anime_name"]}
 
   ENV["CHANNEL_ID"].split(",").each do |chan_id|
     @threads << Thread.new do
-      voice_chan = @bot.channel(chan_id)
-      voice = @bot.voice_connect(voice_chan)
-      voice.adjust_average = false
-      voice.adjust_offset = (ENV["ADJUST_OFFSET"] || 5).to_i
-      voice.adjust_interval = (ENV["ADJUST_INTERVAL"] || 50).to_i
-      voice.volume = @volume
-      log "Joined #{chan_id}"
-      voice.play_file(ENV["STREAM_URL"] || "http://listen.moe:9999/stream")
-      log "Exited #{chan_id}"
+      log "#{chan_id} Joined"
+      begin
+        voice_chan = @bot.channel(chan_id)
+        voice = @bot.voice_connect(voice_chan)
+        voice.adjust_average = false
+        voice.adjust_offset = (ENV["ADJUST_OFFSET"] || 5).to_i
+        voice.adjust_interval = (ENV["ADJUST_INTERVAL"] || 50).to_i
+        voice.volume = @volume
+        loop do
+          log "#{chan_id} Started"
+          voice.play_file(ENV["STREAM_URL"] || "http://listen.moe:9999/stream")
+          log "#{chan_id} Stopped"
+          break if !@running
+        end
+      rescue => e
+        log "#{chan_id}: #{e.to_s.gsub("`", "'")}\n```\n#{e.backtrace.join("\n")}\n```", :error
+      end
+      log "#{chan_id} Exited"
     end
   end
 
@@ -113,7 +122,17 @@ end
 
 def stop_bot!
   @bot.stop
-  @thread.each {|x| x.join}
+
+  procs = `ps -A | grep ffmpeg`.split("\n").map{|x| x.split(" ")[0]}
+  procs.each do |x|
+    output = `kill #{x}`
+    log "Killed #{x}: #{output.inspect}", :pkill
+  end
+
+  old = @running
+  @running = false
+  @threads.each {|x| x.join}
+  @running = old
 end
 
 def ensure_bot!(client_id)
@@ -132,7 +151,7 @@ def ensure_bot!(client_id)
     begin
       @bot.stop
     rescue => e
-      log e, :error
+      log "#{e.to_s.gsub("`", "'")}\n```\n#{e.backtrace.join("\n")}\n```", :error
     end
     log "Bot not running. Starting up."
     start_bot!(client_id)
@@ -158,7 +177,7 @@ def ensure_bot!(client_id)
           @current_song["start_time"] = Time.now.to_i
           @bot.game = "#{@current_song["song_name"]} by #{@current_song["artist_name"]}"
         rescue => e
-          log e, :error
+          log "#{e.to_s.gsub("`", "'")}\n```\n#{e.backtrace.join("\n")}\n```", :error
         end
       end
     end
@@ -170,12 +189,15 @@ def ensure_bot!(client_id)
   end
 end
 
-EM.run {
-  ensure_bot!(client_id)
-  EventMachine::PeriodicTimer.new(10) do
+loop do
+  EM.run {
     ensure_bot!(client_id)
-    @bot.game = "#{@current_song["artist_name"]} - #{@current_song["song_name"]}"
-  end
-}
+    EventMachine::PeriodicTimer.new(10) do
+      ensure_bot!(client_id)
+      @bot.game = "#{@current_song["artist_name"]} - #{@current_song["song_name"]}"
+    end
+  }
 
-stop_bot!
+  stop_bot!
+  break if !@running
+end
